d9afeb087225403b0052996c8abcdee01c5de4a6
ybieul <biel782003@gmail.com>
2025-09-09 17:32:53 -0300

    melhoras no relatorio e financeiro> filtro de periodo com calendario, ptbr

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -3,6 +3,7 @@
 import * as React from "react"
 import { ChevronLeft, ChevronRight } from "lucide-react"
 import { DayPicker } from "react-day-picker"
+import { ptBR } from "date-fns/locale"
 
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
@@ -13,10 +14,12 @@ function Calendar({
   className,
   classNames,
   showOutsideDays = true,
+  locale,
   ...props
 }: CalendarProps) {
   return (
     <DayPicker
+      locale={locale ?? ptBR}
       showOutsideDays={showOutsideDays}
       className={cn("p-3", className)}
       classNames={{
